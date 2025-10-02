17c49ae4ac21d670f0694e6018c999e69af4c44c
Wenyuan Huang <157926136+whuang369@users.noreply.github.com>
2025-10-02 11:54:40 -0500

    add a testing script

---
+++ exec/ppo_exps/mt10-rand/famo_single_seed.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+for e in 24576
+do
+	task_counts=[2458,2458,2458,2458,2458,2458,2457,2457,2457,2457]
+	t=$(( (10000000 + (e * 32) - 1) / (e * 32) ))
+	for i in 47
+	do
+		cmd="python isaacgymenvs/train.py \
+			task_id=[4,16,17,18,28,31,38,40,48,49] \
+			task_counts=$task_counts \
+			num_envs=$e \
+			task=meta-world-v2 \
+			fixed=False \
+			reward_scale=100 \
+			termination_on_success=False \
+			experiment=05_09_ppo_famo_mt10_rand_envs_${e}_seed_$i \
+			train=meta-world-mt10-famo-PPO \
+			seed=$i \
+			wandb_activate=True \
+			headless=True \
+			sim_device=cuda:0 \
+			rl_device=cuda:0 \
+			record_videos=False \
+			max_iterations=$t"
+		echo $cmd
+		$cmd
+	done
+done
+
