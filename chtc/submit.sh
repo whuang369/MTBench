0fe6fc156ac7d372b7c4eff2174c21819bee3465
Wenyuan Huang <157926136+whuang369@users.noreply.github.com>
2025-10-27 13:04:21 -0500

    update job submit script

--- chtc/submit.sh
+++ chtc/submit.sh
@@ -1,8 +1,6 @@
 results_dir=results/${1}
 log_dir=logs/${1}
 commands_file=commands/${1}.txt
-commands_file_tmp=commands/${1}_tmp.txt
-tr ' \n' '*@' < "$commands_file" > "$commands_file_tmp"
 
 mkdir -p ${results_dir}
 mkdir -p ${log_dir}
@@ -10,8 +8,6 @@ mkdir -p ${log_dir}
 condor_submit job.sub \
   results_dir=${results_dir} \
   log_dir=${log_dir} \
-  commands_file=${commands_file_tmp} \
+  commands_file=${commands_file} \
   job_length=${2} \
-  gpu_mem=${3} \
-
-rm $commands_file_tmp
+  gpu_mem=${3}
